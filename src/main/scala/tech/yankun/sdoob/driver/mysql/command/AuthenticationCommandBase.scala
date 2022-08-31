d033d5529f3bfd611c941e71a405dbf3c729465a
yankun <1939810907@qq.com>
2022-08-31 23:05:55 +0800

    mysql AuthenticationCommandBase

--- src/main/scala/tech/yankun/sdoob/driver/mysql/command/AuthenticationCommandBase.scala
+++ src/main/scala/tech/yankun/sdoob/driver/mysql/command/AuthenticationCommandBase.scala
@@ -1,10 +1,13 @@
 package tech.yankun.sdoob.driver.mysql.command
 
+import io.netty.buffer.ByteBuf
 import tech.yankun.sdoob.driver.Command
+import tech.yankun.sdoob.driver.mysql.MySQLCollation
 
 class AuthenticationCommandBase(val username: String,
                                 val password: String,
                                 val database: String,
-                                val collation: String) extends Command {
+                                val collation: MySQLCollation, val serverRsaPublicKey: ByteBuf,
+                                val connectionAttributes: Map[String, String]) extends Command {
 
 }
