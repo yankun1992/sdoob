4ae7d8c0da282f4799c2ab7d11d59cd4eee9935e
yankun <1939810907@qq.com>
2022-09-23 23:05:19 +0800

    feat: Reader

--- src/main/scala/tech/yankun/sdoob/reader/Reader.scala
+++ src/main/scala/tech/yankun/sdoob/reader/Reader.scala
@@ -1,7 +1,12 @@
 package tech.yankun.sdoob.reader
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.log4s.getLogger
+import tech.yankun.sdoob.args.AppArgs
+import tech.yankun.sdoob.driver.SqlConnectOptions
 
-abstract class Reader(val spark: SparkSession) {
-  def read(): DataFrame
+abstract class Reader(val connectOptions: SqlConnectOptions, val appArgs: AppArgs) extends Serializable {
+  protected val logger = getLogger
+
+  def read(spark: SparkSession): DataFrame
 }
